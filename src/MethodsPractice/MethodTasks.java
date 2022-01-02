e0479f898436f76130a89fc55840bd21720db159
berkanhoti <berkanhoti.sonnecto@gmail.com>
2022-01-02 17:38:55 +0100

    Commit 3

---
+++ src/MethodsPractice/MethodTasks.java
@@ -0,0 +1,20 @@
+package MethodsPractice;
+
+import java.util.Locale;
+
+public class MethodTasks {
+    public static void main(String[] args) {
+
+        multiply(int a, int b);
+    }
+
+
+
+
+    public static void multiply(int a, int b) {
+
+        System.out.println(a / b);
+
+
+    }
+}
